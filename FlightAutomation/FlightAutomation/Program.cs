5c384db8eaa407b42631d9d58afa7fc3f88ce2a8
Kuniyal <umesh.kuniyal@siemens.com>
2020-06-28 23:06:58 +0530

    first checkin

---
+++ FlightAutomation/FlightAutomation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FlightAutomation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
